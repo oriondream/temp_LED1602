4ade7fb2b2c2d711ccc60dff13e86b74d9e07232
Vu Nguyen <nguyen.minh.vu@gmail.com>
2019-08-12 19:15:59 +1000

    Degree symbol

--- temp_LED1602.ino
+++ temp_LED1602.ino
@@ -1,45 +1,17 @@
 #include <Wire.h>
 
 /*
-  LiquidCrystal Library - Hello World
-
-  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
-  library works with all LCD displays that are compatible with the
-  Hitachi HD44780 driver. There are many of them out there, and you
-  can usually tell them by the 16-pin interface.
-
-  This sketch prints "Hello World!" to the LCD
-  and shows the time.
-
   The circuit:
-   LCD RS pin to digital pin 12
-   LCD Enable pin to digital pin 11
-   LCD D4 pin to digital pin 5
-   LCD D5 pin to digital pin 4
-   LCD D6 pin to digital pin 3
-   LCD D7 pin to digital pin 2
-   LCD R/W pin to ground
-   LCD VSS pin to ground
-   LCD VCC pin to 5V
-   10K resistor:
-   ends to +5V and ground
-   wiper to LCD VO pin (pin 3)
-
-  Library originally added 18 Apr 2008
-  by David A. Mellis
-  library modified 5 Jul 2009
-  by Limor Fried (http://www.ladyada.net)
-  example added 9 Jul 2009
-  by Tom Igoe
-  modified 22 Nov 2010
-  by Tom Igoe
-  modified 7 Nov 2016
-  by Arturo Guadalupi
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
+   LCD VSS -> ground
+   LCD VCC -> 5V
+   LCD VO -> poty slider
+   LCD RS -> Arduino 9
+   LCD RW -> ground
+   LCD E -> Arduino 8
+   LCD D4 -> Arduino 5
+   LCD D5 -> Arduino 4
+   LCD D6 -> Arduino 3
+   LCD A -> Arduino 2
 */
 #include <DS3231.h>
 #include <Wire.h>
@@ -79,10 +51,21 @@ unsigned long time;
 
 float t;
 
+byte degree_symbol[8] = {
+  B00100,
+  B01010,
+  B00100,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+};
+
 void display_temp()
 {
     lcd.setCursor(0, 1);
-    lcd.print(t);   
+    lcd.print(t);
+    lcd.write(byte(0));
 }
 
 String lcd_time;
@@ -233,7 +216,7 @@ void setup()
 {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-
+  lcd.createChar(0, degree_symbol);
   // Print a message to the LCD.
   lcd.setCursor(0, 0);
   lcd.print("Temp");
